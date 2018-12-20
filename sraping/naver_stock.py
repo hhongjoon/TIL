7690f320e5b567e5b59c6c661a28c5e0cc80c624
hongjoon <sungsung129@gmail.com>
2018-12-20 17:51:28 +0900

    1220_page+scraping

--- sraping/naver_stock.py
+++ sraping/naver_stock.py
@@ -3,7 +3,15 @@ from bs4 import BeautifulSoup
 
 req = requests.get("https://m.stock.naver.com/marketindex/index.nhn").text
 soup = BeautifulSoup(req, 'html.parser')
-aa = soup.select("#marketindexLastList .stock_dn .item_wrp .stock_item")
+#aa = soup.select("#marketindexLastList .stock_dn .item_wrp .stock_item")
+aa = soup.select("#marketindexLastList .item_wrp .stock_item")
+bb = soup.select("#marketindexLastList .price_wrp .stock_price")
 
 for i in aa:
     print(i.text)
+
+for a in bb:
+    print(a.text)
+
+for i in range(len(aa)):
+    print("{}     {}".format(aa[i].text, bb[i].text))
