33b7b9d14733a799c32976ce13666e14681c8b75
hhongjoon <sungsung129@gmail.com>
2019-01-12 21:51:34 +0900

    190112AL

---
+++ HJ_AL/b10799.py
@@ -0,0 +1,15 @@
+# 10799
+aa = input()
+size = []
+piece = 0
+for i in range(len(aa)):
+    if aa[i] == '(':
+        size.append('(')
+    else:
+        if aa[i-1] == '(':  # 레이저라면  # 칸을 나누었을 때 앞에 있는 '(' 갯수로 판단
+            size.pop()
+            piece += len(size)
+        else:               # 레이저 아닐 때 , 괄호를 닫으면서 1개 생김 (규칙)
+            size.pop()
+            piece += 1
+print(piece)
